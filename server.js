d4a47962bc80e1614df80fda2d4f611c6c661681
Matias Damonte <mdamonte88@hotmail.com>
2021-03-12 12:43:58 -0300

    Reduces the complexity of server.js

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 "use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+var app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
