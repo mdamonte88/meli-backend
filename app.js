bf97c3677f085433040d87013297ce96fba56fe3
Matias Damonte <mdamonte88@hotmail.com>
2021-03-14 20:43:24 -0300

    Adds CORS module

--- app.js
+++ app.js
@@ -3,10 +3,14 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 connectDB();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/api', (req, res) => res.send('API Running'));
 
 module.exports = app;
