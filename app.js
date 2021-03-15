e60092f282b5994af1443c64213d6696dc7b5061
Matias Damonte <mdamonte88@hotmail.com>
2021-03-15 17:00:54 -0300

    Adds Morgan module to show the requests

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const connectDB = require('./config/db');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const logger = require("morgan")
 const itemsRoutes = require('./routes/api/items_routes');
 connectDB();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(logger("dev"));
 
 app.get('/api', (req, res) => res.send('API Running'));
 app.use('/api/items', itemsRoutes);
